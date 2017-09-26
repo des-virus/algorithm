fd88837d5465fbf471c89dea400baa8185cb8546
D Virus <duymanhtemp@gmail.com>
2017-09-26 11:49:31 +0700

    ms

---
+++ src/AI/Test.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI;
+
+/**
+ *
+ * @author thuctap02
+ */
+public class Test {
+
+    public int size = 3;
+    public int POINT_NEAR_ONE = 10;
+    public int POINT_BONUS = 10;
+    public int ENEMY_VALUE = 1;
+    public int MY_VALUE = 2;
+    public int a[][];
+
+    public Test() {
+        a = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                a[i][j] = 0;
+            }
+        }
+
+        a[0][2] = 1;
+        a[2][0] = 1;
+    }
+
+    public int calHorizontalPoint(int i, int j) {
+        int totalPoint = 0;
+        int pointToLeft = 0;
+        int pointToRight = 0;
+
+        for (int k = j; k < size; k++) {
+            if (a[i][k] == ENEMY_VALUE) {
+                pointToRight++;
+            }
+        }
+
+        for (int k = j; k >= 0; k--) {
+            if (a[i][k] == ENEMY_VALUE) {
+                pointToLeft++;
+            }
+        }
+
+        totalPoint = pointToLeft + pointToRight;
+        if (totalPoint > 1) {
+            totalPoint++;
+        }
+        return POINT_NEAR_ONE * totalPoint;
+    }
+
+    public int calVerticalPoint(int i, int j) {
+        int totalPoint = 0;
+        int pointToTop = 0;
+        int pointToBottom = 0;
+
+        for (int k = i; k < size; k++) {
+            if (a[k][j] == ENEMY_VALUE) {
+                pointToTop++;
+            }
+        }
+
+        for (int k = i; k >= 0; k--) {
+            if (a[k][j] == ENEMY_VALUE) {
+                pointToBottom++;
+            }
+        }
+
+        totalPoint = pointToTop + pointToBottom;
+        if (totalPoint > 1) {
+            totalPoint++;
+        }
+        return POINT_NEAR_ONE * totalPoint;
+    }
+
+    public int calMainDiagonalPoint(int i, int j) {
+        int totalPoint = 0;
+        int pointToTop = 0;
+        int pointToBottom = 0;
+        int k, l;
+
+        for (k = i, l = j; k >= 0 && l >= 0; k--, l--) {
+            if (a[k][l] == ENEMY_VALUE) {
+                pointToTop++;
+            }
+        }
+
+        for (k = i, l = j; k < size && l < size; k++, l++) {
+            if (a[k][l] == ENEMY_VALUE) {
+                pointToBottom++;
+            }
+        }
+
+        totalPoint = pointToTop + pointToBottom;
+        if (totalPoint > 1) {
+            totalPoint++;
+        }
+        return POINT_NEAR_ONE * totalPoint;
+    }
+
+    public int calDiagonalPoint(int i, int j) {
+           int totalPoint = 0;
+        int pointToTop = 0;
+        int pointToBottom = 0;
+        int k, l;
+
+        for (k = i, l = j; k >= 0 && l < size; k--, l++) {
+            if (a[k][l] == ENEMY_VALUE) {
+                pointToTop++;
+            }
+        }
+
+        for (k = i, l = j; k < size && l >= 0; k++, l--) {
+            if (a[k][l] == ENEMY_VALUE) {
+                pointToBottom++;
+            }
+        }
+
+        totalPoint = pointToTop + pointToBottom;
+        if (totalPoint > 1) {
+            totalPoint++;
+        }
+        return POINT_NEAR_ONE * totalPoint;
+        
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+
+        for (int i = 0; i < t.size; i++) {
+            for (int j = 0; j < t.size; j++) {
+                if (t.a[i][j] == 0) {
+                    int hori = t.calHorizontalPoint(i, j);
+                    int vert = t.calVerticalPoint(i, j);
+                    int md = t.calMainDiagonalPoint(i, j);
+                    int d = t.calDiagonalPoint(i, j);
+
+                    int max = Math.max(hori, vert);
+                    max = Math.max(md, max);
+                    max = Math.max(d, max);
+                    
+                    int total = hori + vert + md + d;
+
+                    System.out.print(total + "--");
+                } else {
+                    System.out.print("x--");
+                }
+            }
+            System.out.println();
+        }
+
+        // System.out.println(t.calVerticalPoint(0, 2));
+    }
+}
---
+++ src/AI/newtest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AI;
+
+/**
+ * ffs
+ *
+ * @author thuctap02
+ */
+public class newtest {
+
+    public static void main(String[] args) {
+        for (int a = 1, b = 1; a < 5 && b < 5; a++, b++) {
+            System.out.println(a + " - " + b);
+        }
+    }
+}
